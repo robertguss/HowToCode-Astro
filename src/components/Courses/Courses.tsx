c2e1eff783e23499c6ff80fafd9d16d982510ee7
Robert Guss <robertguss@pm.me>
2021-10-02 11:04:08 -0400

    added udemy courses to course page

--- src/components/Courses/Courses.tsx
+++ src/components/Courses/Courses.tsx
@@ -9,7 +9,7 @@ export default function Courses() {
           <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0">
             <div>
               <div>
-                <span className="h-12 w-12 rounded-md flex items-center justify-center bg-indigo-600">
+                <span className="h-12 w-12 rounded-md flex items-center justify-center bg-blue-600">
                   <InboxIcon
                     className="h-6 w-6 text-white"
                     aria-hidden="true"
@@ -18,20 +18,18 @@ export default function Courses() {
               </div>
               <div className="mt-6">
                 <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">
-                  Stay on top of customer support
+                  Webpack 5 Fundamentals
                 </h2>
                 <p className="mt-4 text-lg text-gray-500">
-                  Semper curabitur ullamcorper posuere nunc sed. Ornare iaculis
-                  bibendum malesuada faucibus lacinia porttitor. Pulvinar
-                  laoreet sagittis viverra duis. In venenatis sem arcu pretium
-                  pharetra at. Lectus viverra dui tellus ornare pharetra.
+                  Learn the latest version of Webpack along with other modern
+                  tools like Babel, Jest, ESLint, & Prettier.
                 </p>
                 <div className="mt-6">
                   <a
                     href="#"
-                    className="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700"
+                    className="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700"
                   >
-                    Get started
+                    Take Course
                   </a>
                 </div>
               </div>
@@ -40,22 +38,24 @@ export default function Courses() {
               <blockquote>
                 <div>
                   <p className="text-base text-gray-500">
-                    &ldquo;Cras velit quis eros eget rhoncus lacus ultrices sed
-                    diam. Sit orci risus aenean curabitur donec aliquet. Mi
-                    venenatis in euismod ut.&rdquo;
+                    &ldquo;Clear and concise! Everything is up to the point and
+                    well explained. Its a dream that this course is free and yet
+                    it covers not only everything one needs to get their webpack
+                    up and running but also lots of answers about the hows and
+                    whys!&rdquo;
                   </p>
                 </div>
                 <footer className="mt-3">
                   <div className="flex items-center space-x-3">
-                    <div className="flex-shrink-0">
+                    {/* <div className="flex-shrink-0">
                       <img
                         className="h-6 w-6 rounded-full"
                         src="https://images.unsplash.com/photo-1509783236416-c9ad59bae472?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80"
                         alt=""
                       />
-                    </div>
+                    </div> */}
                     <div className="text-base font-medium text-gray-700">
-                      Marcia Hill, Digital Marketing Manager
+                      Arun Basnet
                     </div>
                   </div>
                 </footer>
@@ -66,8 +66,8 @@ export default function Courses() {
             <div className="pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
               <img
                 className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
-                src="https://tailwindui.com/img/component-images/inbox-app-screenshot-1.jpg"
-                alt="Inbox user interface"
+                src="/images/courses/webpack-5.png"
+                alt="Webpack 5 Course Cover"
               />
             </div>
           </div>
@@ -78,7 +78,7 @@ export default function Courses() {
           <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-32 lg:max-w-none lg:mx-0 lg:px-0 lg:col-start-2">
             <div>
               <div>
-                <span className="h-12 w-12 rounded-md flex items-center justify-center bg-indigo-600">
+                <span className="h-12 w-12 rounded-md flex items-center justify-center bg-blue-600">
                   <SparklesIcon
                     className="h-6 w-6 text-white"
                     aria-hidden="true"
@@ -87,31 +87,56 @@ export default function Courses() {
               </div>
               <div className="mt-6">
                 <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">
-                  Better understand your customers
+                  Learn Webpack 2 from scratch
                 </h2>
                 <p className="mt-4 text-lg text-gray-500">
-                  Semper curabitur ullamcorper posuere nunc sed. Ornare iaculis
-                  bibendum malesuada faucibus lacinia porttitor. Pulvinar
-                  laoreet sagittis viverra duis. In venenatis sem arcu pretium
-                  pharetra at. Lectus viverra dui tellus ornare pharetra.
+                  Learn how to build web applications using Javascript's most
+                  popular build tool... Webpack 2
                 </p>
                 <div className="mt-6">
                   <a
                     href="#"
-                    className="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700"
+                    className="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700"
                   >
-                    Get started
+                    Take Course
                   </a>
                 </div>
               </div>
+            </div>
+            <div className="mt-8 border-t border-gray-200 pt-6">
+              <blockquote>
+                <div>
+                  <p className="text-base text-gray-500">
+                    &ldquo;Highly recommend! You get updated with the new way of
+                    doing js related programming with actual tools to speed up
+                    the boring and repeating tasks. The instructor is good and
+                    fluent. Free here doesn't mean cheap. High value
+                    inside.&rdquo;
+                  </p>
+                </div>
+                <footer className="mt-3">
+                  <div className="flex items-center space-x-3">
+                    {/* <div className="flex-shrink-0">
+                      <img
+                        className="h-6 w-6 rounded-full"
+                        src="https://images.unsplash.com/photo-1509783236416-c9ad59bae472?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80"
+                        alt=""
+                      />
+                    </div> */}
+                    <div className="text-base font-medium text-gray-700">
+                      Octavian ROMINU
+                    </div>
+                  </div>
+                </footer>
+              </blockquote>
             </div>
           </div>
           <div className="mt-12 sm:mt-16 lg:mt-0 lg:col-start-1">
             <div className="pr-4 -ml-48 sm:pr-6 md:-ml-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
               <img
                 className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none"
-                src="https://tailwindui.com/img/component-images/inbox-app-screenshot-2.jpg"
-                alt="Customer profile user interface"
+                src="/images/courses/webpack.png"
+                alt="Webpack 2 Course Cover"
               />
             </div>
           </div>
