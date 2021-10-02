dfe5273ce28b3932325531adbeb7d9baed0108bb
Robert Guss <robertguss@pm.me>
2021-10-02 10:02:10 -0400

    fixed z-index of mobile menu

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -330,7 +330,7 @@ export default function Nav() {
       >
         <Popover.Panel
           focus
-          className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+          className="mobile-menu absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden z-10"
         >
           <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
             <div className="pt-5 pb-6 px-5">
