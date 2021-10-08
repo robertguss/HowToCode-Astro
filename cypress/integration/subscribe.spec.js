6c0e3fb0d57a8d0fb32d1986cd70cd7da9b8ca50
Robert Guss <robertguss@pm.me>
2021-10-08 13:18:54 -0400

    added tests for subscribe input

---
+++ cypress/integration/subscribe.spec.js
@@ -0,0 +1,24 @@
+describe("Subscribe", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/subscribe");
+  });
+
+  // https://glebbahmutov.com/blog/form-validation-in-cypress/
+  it("does NOT accept an invalid email", () => {
+    cy.get("#tlemail,input:valid").should("have.length", 1);
+
+    cy.getBySel("subscribe-input").type("john@");
+    cy.getBySel("subscribe-submit-btn").click();
+
+    cy.get("#tlemail,input:invalid").should("have.length", 1);
+  });
+
+  it.only("does accept an invalid email", () => {
+    cy.get("#tlemail,input:valid").should("have.length", 1);
+
+    cy.getBySel("subscribe-input").type("john@test.com");
+    cy.getBySel("subscribe-submit-btn").click();
+
+    cy.get("#tlemail,input:valid").should("have.length", 1);
+  });
+});
