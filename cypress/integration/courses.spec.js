80764815c1b8da14aa5e32c97d222c2bde508234
Robert Guss <robertguss@pm.me>
2021-10-08 13:42:00 -0400

    added tests for /courses

---
+++ cypress/integration/courses.spec.js
@@ -0,0 +1,28 @@
+describe("/courses", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/courses");
+  });
+
+  it("renders the correct header", () => {
+    cy.getBySel("header-title").contains("Courses");
+  });
+
+  it("renders 2 courses", () => {
+    cy.getBySel("courses").within(() => {
+      cy.getBySel("course-1").should("be.visible");
+      cy.getBySel("course-2").should("be.visible");
+    });
+  });
+
+  it("renders the correct course titles", () => {
+    cy.getBySel("courses").within(() => {
+      cy.getBySel("course-1").within(() => {
+        cy.getBySel("course-title").contains("Webpack 5 Fundamentals");
+      });
+
+      cy.getBySel("course-2").within(() => {
+        cy.getBySel("course-title").contains("Learn Webpack 2 from scratch");
+      });
+    });
+  });
+});
