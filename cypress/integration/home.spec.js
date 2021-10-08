6dc0ee6d41c98d7db0aa6b45070a9149972cc0f1
Robert Guss <robertguss@pm.me>
2021-10-08 13:42:44 -0400

    added header test to home

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -3,6 +3,10 @@ describe("Home Page", () => {
     cy.visit("http://localhost:3000");
   });
 
+  it("renders the correct header", () => {
+    cy.getBySel("header-title").contains("How To Code");
+  });
+
   it("the See All link at the bottom of the posts links to the /posts page", () => {
     cy.getBySel("see-all-link").contains("See All").click();
     cy.location("pathname").should("equal", "/posts");
