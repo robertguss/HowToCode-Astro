c9b70a0cdff19b84b1286b61818271c416de7e66
Robert Guss <robertguss@pm.me>
2021-10-25 10:14:47 -0400

    removed react rendering from nav comp

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@
 // @ts-check
 export default /** @type {import('astro').AstroUserConfig} */ ({
   // Enable the Preact renderer to support Preact JSX components.
-  renderers: ["@astrojs/renderer-react"],
+  renderers: ["@astrojs/renderer-preact"],
   devOptions: {
     tailwindConfig: "./tailwind.config.js",
   },
